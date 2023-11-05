84dd4547d51ea6604d29dd6a424f7cefa389e747
vd7 <me@vd7.io>
2023-11-04 20:05:21 -0800

    block

--- index.ts
+++ index.ts
@@ -6,15 +6,23 @@ class Transaction {
     public payer: string, // public key
     public payee: string // public key    
   ){}
-
   toString() {
     return JSON.stringify(this);
   }
-
 }
 
 class Block {
-  
+  constructor(
+    public prevHash: string,
+    public transaction: Transaction,
+    public ts = Date.now()
+  ){}
+  get hash() {
+    const str = JSON.stringify(this);
+    const hash = crypto.createHash('SHA256');
+    hash.update(str).end()
+    return hash.digest('hex')
+  }
 }
 
 class Chain {
