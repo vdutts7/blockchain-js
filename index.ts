b182519f12735b9c25acbff35ebee77442ced4b2
vd7 <me@vd7.io>
2024-10-31 19:48:12 -0700

    Wallet

--- index.ts
+++ index.ts
@@ -38,12 +38,45 @@ class Chain {
     return this.chain[this.chain.length - 1];
   }
 
-  addBlock(transaction: Transaction, senderPublicKey: string, signature: String) {
-    
+  addBlock(transaction: Transaction, senderPublicKey: string, signature: Buffer) {
+    const newBlock  = new Block(this.lastBlock.hash, transaction);
+    this.chain.push(newBlock);
+
+    const verifier = crypto.createVerify('SHA256');
+    verifier.update(transaction.toString());
+
+    const isValid =  verifier.verify(senderPublicKey, signature);
+
+    if (isValid) {
+      const newBlock = new Block(this.lastBlock.hash, transaction);
+      this.chain.push(newBlock);
+    }
   }
   
 }
 
 class Wallet {
-  
+  public publicKey: string;
+  public privateKey: string;
+
+  constructor() {
+    const keypair = crypto.generateKeyPairSync('rsa', {
+      modulusLength: 2048,
+      publicKeyEncoding: { type: 'spki', format: 'pem'},
+      privateKeyEncoding: { type: 'pkcs8', format: 'pem'},
+    });
+
+    this.privateKey = keypair.privateKey;
+    this.publicKey = keypair.publicKey;
+  }
+
+  sendMoney(amount: number, payeePublicKey: string) {
+    const transaction = new Transaction(amount, this.publicKey, payeePublicKey);
+
+    const sign = crypto.createSign('SHA256');
+    sign.update(transaction.toString()).end();
+
+    const signature = sign.sign(this.privateKey);
+    Chain.instance.addBlock(transaction, this.publicKey, signature);
+  }
 }
